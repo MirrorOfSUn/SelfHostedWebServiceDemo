9ca8da35b8a8b46411f2b8b12331ba7861ec24b3
Mirror Sun <MirrorOfSUn@gmail.com>
2019-03-27 18:40:41 -0400

    first upload

---
+++ SelfHostedWebServiceDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+// MS: https://docs.microsoft.com/en-us/aspnet/web-api/overview/older-versions/self-host-a-web-api
+
+namespace SelfHostedWebServiceDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new HttpSelfHostConfiguration("http://27.0.0.1:8080");
+
+			config.Routes.MapHttpRoute(
+				"API Default", "api/{controller}/{id}",
+				new { id = RouteParameter.Optional });
+
+			using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+			{
+				server.OpenAsync().Wait();
+				Console.WriteLine("Press Enter to quit.");
+				Console.ReadLine();
+			}
+		}
+	}
+}
