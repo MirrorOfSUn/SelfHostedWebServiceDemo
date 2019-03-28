16d841741f1f9ba49f4203990d3d70657b900fe4
Mirror Sun <MirrorOfSUn@gmail.com>
2019-03-28 17:32:01 -0400

    xml -> json

--- SelfHostedWebServiceDemo/Program.cs
+++ SelfHostedWebServiceDemo/Program.cs
@@ -19,6 +19,9 @@ namespace SelfHostedWebServiceDemo
 				"API Default", "api/{controller}/{id}",
 				new { id = RouteParameter.Optional });
 
+			var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+			config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
 			using (HttpSelfHostServer server = new HttpSelfHostServer(config))
 			{
 				server.OpenAsync().Wait();
